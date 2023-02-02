4d98d41dc74e8b51c360c91344cb7566c3d546a9
Friedrich Wilken <friedrich@pop-os.localdomain>
2023-02-02 15:50:19 +0100

    Move model to `/pkg/models/hjkl`.

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+
+    "github.com/friedrichwilken/fd/pkg/models/hjkl"
+)
 
 func main() {
-    fmt.Println("lol")
+	p := tea.NewProgram(hjkl.New())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
